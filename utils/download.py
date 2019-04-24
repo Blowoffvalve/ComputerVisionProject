9221f2d642d9889c200aca3f4fa108917eb7af72
Blowoffvalve <osunkoyaopeoluwa@gmail.com>
2019-04-24 12:32:04 -0500

    Added a imagesPerBreed parameter to utils/download.py

--- utils/download.py
+++ utils/download.py
@@ -4,11 +4,14 @@ extension = ".csv"
 breeds = []
 path = Path("../Data/")
 breeds += [each for each in os.listdir(path) if each.endswith(extension)]
+imagesPerBreed = 250
+
+
 for breed in breeds:
 	print("Retrieving images for {} breed".format(breed.split(extension)[0]))
 	dest = path/breed.split(extension)[0]
 	dest.mkdir(parents=True, exist_ok=True)
-	download_images(path/breed, dest, max_pics = 1, max_workers=0)
+	download_images(path/breed, dest, max_pics = imagesPerBreed, max_workers=0)
 	
 photoExtension = [".jpg", ".JPG", ".png", ".jpeg", ".PNG"]
 for dir in os.listdir(path):
