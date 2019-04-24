c02e2f3480a6876407a3212d5f2bc2e72a76e5d7
Blowoffvalve <osunkoyaopeoluwa@gmail.com>
2019-04-24 12:55:01 -0500

    Modified utils/download.py to remove corrupt images

--- utils/download.py
+++ utils/download.py
@@ -1,24 +1,36 @@
 from fastai.vision import *
 import os
+from PIL import Image
+
+
 extension = ".csv"
 breeds = []
 path = Path("../Data/")
 breeds += [each for each in os.listdir(path) if each.endswith(extension)]
 imagesPerBreed = 250
 
-
+#Download files
 for breed in breeds:
 	print("Retrieving images for {} breed".format(breed.split(extension)[0]))
 	dest = path/breed.split(extension)[0]
 	dest.mkdir(parents=True, exist_ok=True)
 	download_images(path/breed, dest, max_pics = imagesPerBreed, max_workers=0)
-	
+
+#Delete unnecessary or invalid files
 photoExtension = [".jpg", ".JPG", ".png", ".jpeg", ".PNG"]
 for dir in os.listdir(path):
 	if os.path.isdir(path/dir):
 		for file in os.listdir(path/dir):
 			filename, fileExtension = os.path.splitext(file)
+			#Remove files with extensions that aren't jpg or png
 			if not fileExtension in photoExtension:
 				print("Removing " + dir+"/"+ file)
 				os.remove(path/dir/file)
+			else:
+				#Remove images that can't be opened by PIL as they're most likely corrupt
+				try:
+					Image.open(path/dir/file)
+				except IOError:
+					os.remove(path/dir/file)
+					print("Removing " + dir+"/"+ file + " Because it is invalid")
 				
