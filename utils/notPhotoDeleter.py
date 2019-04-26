8f6ee8a508de4b670588f8fe35a7dfa318fb95aa
Blowoffvalve <osunkoyaopeoluwa@gmail.com>
2019-04-25 20:50:25 -0500

    Started the Jupyter notebook

--- utils/notPhotoDeleter.py
+++ utils/notPhotoDeleter.py
@@ -1,13 +1,25 @@
-from fastai.vision import *
+from fastai.vision import Path
 import os
+from PIL import Image
 
 path = Path("../Data/")
+
+#Delete unnecessary or invalid files
 photoExtension = [".jpg", ".JPG", ".png", ".jpeg", ".PNG"]
 for dir in os.listdir(path):
 	if os.path.isdir(path/dir):
 		for file in os.listdir(path/dir):
+			#print("Examining " + dir+"/"+ file)
 			filename, fileExtension = os.path.splitext(file)
+			#Remove files with extensions that aren't jpg or png
 			if not fileExtension in photoExtension:
 				print("Removing " + dir+"/"+ file)
 				os.remove(path/dir/file)
+			else:
+				#Remove images that can't be opened by PIL as they're most likely corrupt
+				try:
+					Image.open(path/dir/file)
+				except IOError:
+					os.remove(path/dir/file)
+					print("Removing " + dir+"/"+ file + " Because it is invalid")
 				
